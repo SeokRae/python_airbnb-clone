fe71a50fa56475eef2f53b656e1a8c79bd9fed8d
김석래 <kslbsh@gmail.com>
2019-12-23 14:46:20 +0900

    2019.12.23 users app

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    # https://www.youtube.com/watch?v=Wn7j5dfbJF4&t=1s
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Fields",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthday",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
--- users/models.py
+++ users/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(AbstractUser):
+
+    """ CUSTOM USER MODEL """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOISES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "ko"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_KRW, "KRW"), (CURRENCY_USD, "USD"))
+
+    avatar = models.ImageField(null=True, blank=True)
+
+    gender = models.CharField(
+        choices=GENDER_CHOISES, max_length=10, null=True, blank=True,
+    )
+
+    bio = models.TextField(default="", blank=True)
+
+    birthday = models.DateField(null=True)
+
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True,
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
+
+    superhost = models.BooleanField(default=False)
