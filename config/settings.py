263fd80c95d481aef2c6923ec15590ebb14d1832
seokrae <kslbsh@gmail.com>
2019-12-24 01:51:51 +0900

    2019.12.24 admin update

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,9 @@ PROJECT_APPS = [
     "rooms.apps.RoomsConfig",
     "core.apps.CoreConfig",
     "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig",
+    "lists.apps.ListsConfig",
+    "conversations.apps.ConversationsConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
--- rooms/admin.py
+++ rooms/admin.py
@@ -15,17 +15,70 @@ class ItemAdmin(admin.ModelAdmin):
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book",)},),
+        (
+            "Spaces",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules",),
+            },
+        ),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("guests", "beds", "bedrooms", "baths",),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
     list_display = (
+        "name",
         "country",
         "city",
+        "price",
         "address",
-        "host",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
     )
 
     list_filter = (
+        "instant_book",
+        "host__superhost",
+        "host__gender",
         "city",
         "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "country",
     )
+
+    search_fields = [
+        "^city",
+        "host__username",
+    ]
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
 
 
 @admin.register(models.Photo)
