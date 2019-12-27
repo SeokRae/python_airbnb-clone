cca058f1843568b02bb527e2f2b4649000738670
seokrae <kslbsh@gmail.com>
2019-12-27 18:32:15 +0900

    2019.12.27 view paging python mode

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
+from math import ceil
 from . import models
 
 # Create your views here.
 
 
 def all_rooms(request):
-    page = int(request.GET.get("page", 1))
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+
     page_size = 10
 
     limit = page * page_size
     offset = limit - page_size
 
     all_rooms = models.Room.objects.all()[offset:limit]
-    return render(request, "rooms/all_rooms.html", context={"rooms": all_rooms})
+    page_count = ceil(models.Room.objects.count() / page_size)
+
+    return render(
+        request,
+        "rooms/all_rooms.html",
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count + 1),
+        },
+    )
