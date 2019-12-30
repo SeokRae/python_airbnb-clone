a1697178cbd4c6e6411bacdef437bf4a13722fe1
seokrae <kslbsh@gmail.com>
2019-12-30 10:09:36 +0900

    2019.12.30 search form page setting

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,7 @@
 # https://docs.djangoproject.com/en/2.2/ref/class-based-views/generic-display/
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django_countries import countries
 from . import models
 
 # Create your views here.
@@ -23,3 +25,48 @@ class RoomDetail(DetailView):
     """ RoomDetail class Definition """
 
     model = models.Room
+
+
+# Function Base View
+def search(request):
+    # Request Parameter
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+
+    s_amenities = request.GET.get("amenities")
+    s_facilities = request.GET.get("facilities")
+
+    # Page Data
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    form = {
+        "city": city,
+        "s_country": country,
+        "s_room_type": room_type,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+    }
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+
+    return render(request, "rooms/search.html", {**form, **choices},)
