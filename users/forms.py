eb47f48a875c3997c24ed44ba2299d3de8793a0a
seokrae <kslbsh@gmail.com>
2019-12-30 19:00:17 +0900

    2019.12.30 login User email validation

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from . import models
 
 
 class LoginForm(forms.Form):
@@ -6,3 +7,15 @@ class LoginForm(forms.Form):
 
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(username=email)
+            return email
+        except models.User.DoesNotExist:
+            raise forms.ValidationError("User Does Not Exist")
+
+    def clean_password(self):
+        print(self.cleaned_data)
+        return self.cleaned_data
