b369de487b39ef93664ad6f10123f138805a4f10
seokrae <kslbsh@gmail.com>
2019-12-31 10:56:26 +0900

    2019.12.31 login validation

--- users/forms.py
+++ users/forms.py
@@ -8,14 +8,16 @@ class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-    def clean_email(self):
+    def clean(self):
         email = self.cleaned_data.get("email")
-        try:
-            models.User.objects.get(username=email)
-            return email
-        except models.User.DoesNotExist:
-            raise forms.ValidationError("User Does Not Exist")
+        password = self.cleaned_data.get("password")
 
-    def clean_password(self):
-        print(self.cleaned_data)
-        return self.cleaned_data
+        try:
+            # TODO 데이터는 Email과 username이 같아야 한다. 여기서는 임시로 email비교
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return self.cleaned_data
+            else:
+                self.add_error("password", forms.ValidationError("Password Error"))
+        except models.User.DoesNotExist:
+            self.add_error("email", self.add_error("Email Error"))
