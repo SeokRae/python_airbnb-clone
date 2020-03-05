4d3fecf89b559397f6acb6bb1fa90202cba94701
seokrae <kslbsh@gmail.com>
2020-03-05 11:12:50 +0900

    2020.03.05 new start

--- core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from rooms import views as room_views
-
-app_name = "core"
-
-# https://docs.djangoproject.com/en/2.2/topics/http/urls/
-urlpatterns = [path("", room_views.HomeView.as_view(), name="home")]
