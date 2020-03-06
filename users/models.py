b5772087627b933cecf2d6d3364950e23ded131f
seokrae <kslbsh@gmail.com>
2020-03-06 15:45:33 +0900

    2020.03.06 admin panel interface

--- users/models.py
+++ users/models.py
@@ -6,4 +6,42 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     """ Custom User Model """
 
-    bio = models.TextField(default="")
+    # Gender Choices literal
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
+    # null=True는 DB에서 적용하는 값, blank=True는 Form에서 적용하는 값
+    # ImageField 사용하려면 Pillow 라이브러리 필요
+    # pipenv install Pillow
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
+    # DateField
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
+    # BooleanField
+    superhost = models.BooleanField(default=False)
