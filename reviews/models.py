b6c20a7efa5aed744e639cc87a41713b7d70c07c
seokrae <kslbsh@gmail.com>
2020-03-09 15:46:28 +0900

    2020.03.09 review app

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from core import models as core_models
 
 # Create your models here.
+
+
+class Review(core_models.TimeStampedModel):
+
+    """ Review Model Definition """
+
+    review = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    cleanliness = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    value = models.IntegerField()
+    # 유저 삭제 시 숙소도 삭제
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    # 숙소 삭제 시 하위 테이블 데이터도 삭제
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        # python3 String 쓰는 방식
+        return f"{self.review} - {self.room}"
