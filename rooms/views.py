40172a4796d18af989bf58bedbd877dc586cf56e
seokrae <kslbsh@gmail.com>
2020-03-17 14:30:59 +0900

    2020.03.17 function based pagination

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from . import models
 
@@ -7,11 +7,16 @@ from . import models
 
 
 def all_rooms(request):
-    page = request.GET.get("page")
-    room_list = models.Room.objects.all()
+    page = request.GET.get("page", 1)
+    # 
+    room_list = models.Room.objects.get_queryset().order_by("id")
     # Paginator(object_list, per_page, orphans=0, allow_empty_first_page=True)
-    paginator = Paginator(room_list, 10)
-    # 잘못된 페이지 번호 처리
-    rooms = paginator.get_page(page)
-    print(rooms.paginator.num_pages)
-    return render(request, "rooms/home.html", {"rooms": rooms},)
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        # 잘못된 페이지 번호 처리
+        pages = paginator.page(page)
+        return render(request, "rooms/home.html", {"pages": pages},)
+    except EmptyPage:  # 페이지 범위 외의 값 입력시 예외처리
+        return redirect("/")
+    except PageNotAnInteger:  # page 파라미터의 값이 Integer가 아닐 경우 예외처리
+        return redirect("/")
