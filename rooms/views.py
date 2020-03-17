192d93142075c2d9e03f9f3784b37c1f7703328d
seokrae <kslbsh@gmail.com>
2020-03-17 14:34:30 +0900

    2020.03.17 apply ListView (class based view)

--- rooms/views.py
+++ rooms/views.py
@@ -1,22 +1,14 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+from django.views.generic import ListView
 from . import models
 
 # Create your views here.
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    # 
-    room_list = models.Room.objects.get_queryset().order_by("id")
-    # Paginator(object_list, per_page, orphans=0, allow_empty_first_page=True)
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        # 잘못된 페이지 번호 처리
-        pages = paginator.page(page)
-        return render(request, "rooms/home.html", {"pages": pages},)
-    except EmptyPage:  # 페이지 범위 외의 값 입력시 예외처리
-        return redirect("/")
-    except PageNotAnInteger:  # page 파라미터의 값이 Integer가 아닐 경우 예외처리
-        return redirect("/")
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
