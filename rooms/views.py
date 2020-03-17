731388db7262c713fce318caefd7a9318bac42cb
seokrae <kslbsh@gmail.com>
2020-03-17 13:35:48 +0900

    2020.03.17 pagination update

--- rooms/views.py
+++ rooms/views.py
@@ -1,27 +1,17 @@
-from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
 from . import models
 
 # Create your views here.
 
 
 def all_rooms(request):
-    page = request.GET.get("page", 1)
-    # 뭔 차이지?
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-
-    return render(
-        request,
-        "rooms/home.html",
-        {
-            "potato": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count),
-        },
-    )
+    page = request.GET.get("page")
+    room_list = models.Room.objects.all()
+    # Paginator(object_list, per_page, orphans=0, allow_empty_first_page=True)
+    paginator = Paginator(room_list, 10)
+    # 잘못된 페이지 번호 처리
+    rooms = paginator.get_page(page)
+    print(rooms.paginator.num_pages)
+    return render(request, "rooms/home.html", {"rooms": rooms},)
