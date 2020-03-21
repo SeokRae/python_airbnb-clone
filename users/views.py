477372065a59ef89b2c28dd63f9b0a3b27633bfb
seokrae <kslbsh@gmail.com>
2020-03-22 02:28:44 +0900

    2020.03.22 login validation

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.views import View
+from django.shortcuts import render
+from . import forms
+
+
+# class-based view
+class LoginView(View):
+
+    # 기본 함수
+    def get(self, request):
+        form = forms.LoginForm(initial={"email": "seok@gmail.com"})
+        return render(request, "users/login.html", {"form": form})
+
+    def post(self, request):
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+        return render(request, "users/login.html", {"form": form})
