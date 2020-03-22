d881f69ac079757b55390b18b47f6ae1e43fc8ee
seokrae <kslbsh@gmail.com>
2020-03-22 20:55:10 +0900

    2020.03.22 authentication

--- users/views.py
+++ users/views.py
@@ -1,13 +1,12 @@
 # Create your views here.
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
 # class-based view
 class LoginView(View):
-
-    # 기본 함수
     def get(self, request):
         form = forms.LoginForm(initial={"email": "seok@gmail.com"})
         return render(request, "users/login.html", {"form": form})
@@ -15,5 +14,20 @@ class LoginView(View):
     def post(self, request):
         form = forms.LoginForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            # 사용자 인증 함수
+            user = authenticate(request, username=email, password=password)
+
+            if user is not None:
+                # 로그인 함수
+                login(request, user)
+                return redirect(reverse("core:home"))
+
         return render(request, "users/login.html", {"form": form})
+
+
+def log_out(request):
+    # 로그아웃 함수
+    logout(request)
+    return redirect(reverse("core:home"))
