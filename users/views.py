9e0a77cc8e6402066e11d8ec6cd6378cb377567b
seokrae <kslbsh@gmail.com>
2020-03-22 23:27:23 +0900

    2020.03.22 loginForm

--- users/views.py
+++ users/views.py
@@ -1,30 +1,29 @@
 # Create your views here.
-from django.views import View
-from django.shortcuts import render, redirect, reverse
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.shortcuts import redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
 # class-based view
-class LoginView(View):
-    def get(self, request):
-        form = forms.LoginForm(initial={"email": "seok@gmail.com"})
-        return render(request, "users/login.html", {"form": form})
+# View -> FormView로 변경 시, get post 함수 차이
+class LoginView(FormView):
 
-    def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get("email")
-            password = form.cleaned_data.get("password")
-            # 사용자 인증 함수
-            user = authenticate(request, username=email, password=password)
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
 
-            if user is not None:
-                # 로그인 함수
-                login(request, user)
-                return redirect(reverse("core:home"))
+    # FIXME 테스트용 default 값 설정
+    initial = {"email": "seok@gmail.com"}
 
-        return render(request, "users/login.html", {"form": form})
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
 
 
 def log_out(request):
