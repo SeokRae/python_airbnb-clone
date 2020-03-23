9a0f08d5810946fdecbdee192ec6f81b0c975e83
seokrae <kslbsh@gmail.com>
2020-03-23 10:39:30 +0900

    2020.03.23 signUp page form

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from . import models
 
 
+# 로그인 Form
 class LoginForm(forms.Form):
 
     email = forms.EmailField()
@@ -21,3 +22,37 @@ class LoginForm(forms.Form):
 
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User does not exist"))
+
+
+# 회원가입 Form
+class SignUpForm(forms.Form):
+    # Form Input
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    email = forms.EmailField()
+    # widget 수정으로 CharField에서 PasswordInput으로 수정
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    # user duplicate check
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+
+        try:
+            # db에 해당 user가 존재함
+            users = models.User.objects.get(email=email)
+            raise forms.ValidationError(f"{users} already exists with that email")
+        except models.User.DoesNotExist:
+            # db에 해당 user가 없는 경우
+            return email
+
+    # password 체크
+    def clean_password(self):
+
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
--- users/views.py
+++ users/views.py
@@ -20,8 +20,10 @@ class LoginView(FormView):
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
         password = form.cleaned_data.get("password")
+        # 인증 함수
         user = authenticate(self.request, username=email, password=password)
         if user is not None:
+            # 로그인 함수
             login(self.request, user)
         return super().form_valid(form)
 
@@ -30,3 +32,15 @@ def log_out(request):
     # 로그아웃 함수
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+
+    initial = {
+        "first_name": "SeokRae",
+        "last_name": "Kim",
+        "email": "seokr@gmail.com",
+    }
