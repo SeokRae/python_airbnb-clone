83f9d16fd43e4672bdff2ab8e41b5ccc896f2eae
seokrae <kslbsh@gmail.com>
2020-04-02 17:02:31 +0900

    2020.04.02 room detail

--- rooms/models.py
+++ rooms/models.py
@@ -121,10 +121,3 @@ class Room(core_models.TimeStampedModel):
     def get_next_four_photos(self):
         photos = self.photos.all()[1:5]
         return photos
-
-    # room detail
-    def get_beds(self):
-        if self.beds == 1:
-            return "1 bed"
-        else:
-            return f"{self.beds} beds"
